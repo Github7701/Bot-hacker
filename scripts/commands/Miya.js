fbf25af0d9c50006b9d5fab5e9621906760b9164
Github7701 <mn8586229@gmail.com>
2024-10-01 17:58:42 +0600

    Create Miya.js

---
+++ scripts/commands/Miya.js
@@ -0,0 +1,64 @@
+module.exports.config = {
+  name: "autotime",
+version: "1.0.0",
+permission: 0,
+credits: "Joy ahmed",
+description: "msg",
+prefix: true, 
+category: "user", 
+usages: "",
+cooldowns: 5,
+dependencies: {
+}
+};
+const nam = [{
+timer: '12:00:00 AM',
+message: ['=== 「𝗖𝗹𝗼𝗰𝗸」  ===\n  --❖-- 𝐌𝐃 Asif Ahmed Niloy(✷‿✷) --❖--\n✢━━━━━━━━━━━━━━━✢\n\nএখন রাত ১১টা বাজে সবাই সুয়ে পড়ো জেগে না থেকে🙅\n\n✢━━━━━━━━━━━━━━━✢\n𝐅𝐛 𝐥𝐢𝐧𝐤:https://www.facebook.com/profile.php?id=100062724886617&mibextid=ZbWKwL\n𝐂𝐫𝐞𝐚𝐭𝐨𝐫 : 𝐌𝐃 Asif Ahmed Niloy (✷‿✷)']
+},
+         {
+timer: '1:00:00 AM',
+message: ['=== 「𝗖𝗹𝗼𝗰𝗸」  ===\n  --❖-- 𝐌𝐃 Asif Ahmed Niloy(✷‿✷) --❖--\n✢━━━━━━━━━━━━━━━✢\n\nএখন রাত ১২টা বেজে এবার হ্যান্ডেল মারা বন্ধ করে ঘুমা🖕\n\n✢━━━━━━━━━━━━━━━✢\n𝐅𝐛 𝐥𝐢𝐧𝐤:https://www.facebook.com/profile.php?id=100062724886617&mibextid=ZbWKwL\n𝐂𝐫𝐞𝐚𝐭𝐨𝐫 : 𝐌𝐃 Asif Ahmed Niloy(✷‿✷)']
+},
+        {
+timer: '2:00:00 AM',
+message: ['=== 「𝗖𝗹𝗼𝗰𝗸」  ===\n  --❖-- 𝐌𝐃 Asif Ahmed Niloy(✷‿✷) --❖--\n✢━━━━━━━━━━━━━━━✢\n\nএখন রাত ১টা বাজে সবাই মনে হয় ঘুমায় গেছে🥱\n\n✢━━━━━━━━━━━━━━━✢\n𝐅𝐛 𝐥𝐢𝐧𝐤:https://www.facebook.com/profile.php?id=100062724886617&mibextid=ZbWKwL\n𝐂𝐫𝐞𝐚𝐭𝐨𝐫 : 𝐌𝐃 Asif Ahmed Niloy (✷‿✷)']
+},
+         {
+timer: '3:00:00 AM',
+message: ['=== 「𝗖𝗹𝗼𝗰𝗸」  ===\n  --❖-- 𝐌𝐃 Asif Ahmed Niloy(✷‿✷) --❖--\n✢━━━━━━━━━━━━━━━✢\n\nএখন রাত ২টা বাজে যারা ছ্যাকা খাইছে তারা জেগে আছে😒\n\n✢━━━━━━━━━━━━━━━✢\n𝐅𝐛 𝐥𝐢𝐧𝐤:https://www.facebook.com/profile.php?id=100062724886617&mibextid=ZbWKwL\n𝐂𝐫𝐞𝐚𝐭𝐨𝐫 : 𝐌𝐃 Asif Ahmed Niloy(✷‿✷']
+},
+         {
+timer: '4:00:00 AM',
+message: ['=== 「𝗖𝗹𝗼𝗰𝗸」  ===\n  --❖-- 𝐌𝐃 Asif Ahmed Niloy(✷‿✷) --❖--\n✢━━━━━━━━━━━━━━━✢\n\nএখন রাত ৩টা বাজে সবাই মনে হয় ঘুমাই🥹আমার ভাই ঘুম আসে না😔\n\n✢━━━━━━━━━━━━━━━✢\n𝐅𝐛 𝐥𝐢𝐧𝐤:https://www.facebook.com/profile.php?id=100062724886617&mibextid=ZbWKwL\n𝐂𝐫𝐞𝐚𝐭𝐨𝐫 : 𝐌𝐃 Asif Ahmed Niloy(✷‿✷']
+},
+         {
+timer: '5:00:00 AM',
+message: ['=== 「𝗖𝗹𝗼𝗰𝗸」  ===\n  --❖-- 𝐌𝐃 Asif Ahmed Niloy(✷‿✷) --❖--\n✢━━━━━━━━━━━━━━━✢\n\nএখন রাত ৪টা বাজে একটু পর ফজরের আযান দিবে নামাজ পড়ে নিও সবাই\n\n✢━━━━━━━━━━━━━━━✢\n𝐅𝐛 𝐥𝐢𝐧𝐤:https://www.facebook.com/profile.php?id=100062724886617&mibextid=ZbWKwL\n𝐂𝐫𝐞𝐚𝐭𝐨𝐫 : MD Asif Ahmed Niloy(✷‿✷) ']
+},
+         {
+timer: '5:05:00 AM',
+message: ['╭──────────────────╮\n  🌸𝐄𝐯𝐞𝐫𝐲 𝐌𝐮𝐬𝐥𝐢𝐦𝐬 𝐈𝐝𝐞𝐧𝐭𝐢𝐭𝐲 🌸\n╰──────────────────╯\n\n𝙽𝚊𝚖𝚎                     : 𝐌𝐮𝐬𝐥𝐢𝐦.\n𝙵𝚊𝚝𝚑𝚎𝚛•𝚜 𝙽𝚊𝚖𝚎    : 𝐀𝐝𝐨𝐦 (আ:)\n𝙲𝚛𝚎𝚊𝚝𝚘𝚛               : 𝐀𝐥𝐥𝐚𝐡\n𝙸𝚍𝚎𝚊𝚕                   : 𝐌𝐮𝐡𝐚𝐦𝐦𝐚𝐝 (সা.) \n𝙷𝚘𝚕𝚢 𝙱𝚘𝚘𝚔           : 𝐐𝐮𝐫𝐚𝐧 \n𝚁𝚎𝚕𝚒𝚐𝚒𝚘𝚗            : 𝐈𝐬𝐥𝐚𝐦 \n𝙸𝚍𝚎𝚗𝚝𝚒𝚝𝚢            : لَا إِلٰهَ إِلَّا الله مُحَمَّدٌ رَسُولُ الله• \n𝙷𝚘𝚋𝚋𝚒𝚎𝚜               : 𝐍𝐚𝐦𝐚𝐳 𝟓 𝐭𝐢𝐦𝐞𝐬 𝐚 𝐝𝐚𝐲 \n𝙿𝚛𝚎𝚜𝚎𝚗𝚝 𝙰𝚍𝚍𝚛𝚎𝚜𝚜           : 𝐃𝐮𝐧𝐢𝐲𝐚 \n𝙿𝚎𝚛𝚖𝚊𝚗𝚎𝚗𝚝 𝙰𝚍𝚍𝚛𝚎𝚜𝚜      : 𝐉𝐚𝐧𝐧𝐚𝐭 (𝐈𝐧 𝐬𝐡𝐚 𝐚𝐥𝐥𝐚𝐡)']
+},
+         {
+timer: '6:00:00 AM',
+message: ['=== 「𝗖𝗹𝗼𝗰𝗸」  ===\n  --❖-- 𝐌𝐃 Asif Ahmed Niloy(✷‿✷) --❖--\n✢━━━━━━━━━━━━━━━✢\n\nএখন ভোর ৫টা বাজে সবাই নামাজ পড়ছো তো?❤️\n\n✢━━━━━━━━━━━━━━━✢\n𝐅𝐛 𝐥𝐢𝐧𝐤:https://www.facebook.com/profile.php?id=100062724886617&mibextid=ZbWKwL\n𝐂𝐫𝐞𝐚𝐭𝐨𝐫 : 𝐌𝐃 Asif Ahmed Niloy(✷‿✷) ']
+},
+         {
+timer: '7:00:00 AM',
+message: ['=== 「𝗖𝗹𝗼𝗰𝗸」  ===\n  --❖-- 𝐌𝐃 Asif Ahmed Niloy(✷‿✷) --❖--\n✢━━━━━━━━━━━━━━━✢\n\nএখন সকাল ৬টা বাজে ঘুম থেকে উঠো দাঁত ব্রাশ করো সবাই\n\n✢━━━━━━━━━━━━━━━✢\n𝐅𝐛 𝐥𝐢𝐧𝐤:https://www.facebook.com/profile.php?id=100062724886617&mibextid=ZbWKwL\n𝐂𝐫𝐞𝐚𝐭𝐨𝐫 : 𝐌𝐃 Asif Ahmed Niloy (✷‿✷)']
+},
+         {
+timer: '8:00:00 AM',
+message: ['=== 「𝗖𝗹𝗼𝗰𝗸」  ===\n  --❖-- 𝐌𝐃 Asif Ahmed Niloy(✷‿✷) --❖--\n✢━━━━━━━━━━━━━━━✢\n\nএখন সকাল ৭টা বাজে সবাই ব্রেকফাস্ট করে নাও😊\n\n✢━━━━━━━━━━━━━━━✢\n𝐅𝐛 𝐥𝐢𝐧𝐤:https://www.facebook.com/profile.php?id=100062724886617&mibextid=ZbWKwL\n𝐂𝐫𝐞𝐚𝐭𝐨𝐫 : 𝐌𝐃 Asif Ahmed Niloy (✷‿✷) ']
+},
+         {
+timer: '9:00:00 AM',
+message: ['=== 「𝗖𝗹𝗼𝗰𝗸」  ===\n  --❖-- 𝐌𝐃 Asif Ahmed Niloy(✷‿✷) --❖--\n✢━━━━━━━━━━━━━━━✢\n\nএখন সকাল ৮ টা বাজে যারা পড়াশোনা করো তারা স্কুলে যাও😚 যারা পড়ে না তারা কাজে যাও😒\n\n✢━━━━━━━━━━━━━━━✢\n𝐅𝐛 𝐥𝐢𝐧𝐤:https://www.facebook.com/profile.php?id=100062724886617&mibextid=ZbWKwL\n𝐂𝐫𝐞𝐚𝐭𝐨𝐫 : 𝐌𝐃 Asif Ahmed Niloy(✷‿✷)']
+},
+         {
+timer: '10:00:00 AM',
+message: ['=== 「𝗖𝗹𝗼𝗰𝗸」  ===\n  --❖-- 𝐌𝐃 Asif Ahmed Niloy(✷‿✷) --❖--\n✢━━━━━━━━━━━━━━━✢\n\nএখন সকাল ৯ টা বাজে মন দিয়ে কাজ করো সবাই❤️\n\n✢━━━━━━━━━━━━━━━✢\n𝐅𝐛 𝐥𝐢𝐧𝐤:https://www.facebook.com/profile.php?id=100062724886617&mibextid=ZbWKwL\n𝐂𝐫𝐞𝐚𝐭𝐨𝐫 : 𝐌𝐃 Asif Ahmed Niloy(✷‿✷)' ]
+},
+         {
+timer: '11:00:00 AM',
+message: ['=== 「𝗖𝗹𝗼𝗰𝗸>  ===\n  --❖-- 𝐌𝐃 Asif Ahmed Niloy(✷‿✷) --❖--\n✢━━━━━━━━━━━━━━━✢\n\nএখন সকাল ১০টা বাজে মিস করছি তোমাদের😔 তাড়াতাড়ি বাড়িতে চলে আসো😒\n\n✢━━━━━━━━━━━━━━━✢\n𝐅𝐛 𝐥𝐢𝐧𝐤:https://www.facebook.com/profile.php?id=100062724886617&mibextid=ZbWKwL\n𝐂𝐫𝐞𝐚𝐭𝐨𝐫 : 𝐌𝐃 Asif Ahmed
